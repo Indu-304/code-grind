592d3f81fde6a69dd1db9dcf7ace356846276c6a
Indu-304 <122859388+Indu-304@users.noreply.github.com>
2024-04-26 02:02:30 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,26 @@
+//Better
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0 || (x!=0 && x%10==0)) return false;
+        int rev = 0;
+        while (x>rev){
+            rev = rev*10 + x%10;
+            x = x/10;
+        }
+        return (x==rev || x==rev/10);
+    }
+}
+
+
+//String
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        int n = s.length();
+
+        for (int i=0; i<n/2; i++) {
+            if (s.charAt(i) != s.charAt(n-i-1)) return false;
+        }
+        return true;
+    }
+}
