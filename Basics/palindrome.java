84768dcc2adb155e5d9c3dd26d0218850e84bab8
Indu-304 <122859388+Indu-304@users.noreply.github.com>
2024-04-27 20:13:05 +0530

    Update palindrome.java

--- Basics/palindrome.java
+++ Basics/palindrome.java
@@ -24,3 +24,14 @@ class Solution {
         return true;
     }
 }
+
+
+//Recursion
+public class Solution {
+    public static boolean isPalindrome(int i, String s){
+        if (i>s.length()/2) return true;
+        if (s.charAt(i) != s.charAt(s.length()-i-1)) return false;
+        return pal(i+1, s);
+    }
+}
+//call: isPalindrome(0,s)
